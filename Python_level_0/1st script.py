0a67d5f7e94434ac257f2ff54fbe5e16575e0a5e
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-12 18:19:26 +0600

    Add files via upload

---
+++ Python_level_0/1st script.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 18:06:05 2021
+
+@author: shahi
+"""
+
+print('I am gonna be a pythonist')
---
+++ Python_level_0/Normal_Calculation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 18:06:05 2021
+
+@author: shahi
+"""
+#Summation
+print(3+4)
+#substraction
+print(10-9)
+#Multiplication
+print(10*11)
+#division
+print(10/2)
+#reminder
+print(10%2)

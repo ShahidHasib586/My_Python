49c57c1fa9ad8698d2bbb68968533c70d17f597f
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-17 21:45:38 +0600

    Add files via upload

---
+++ Level_1/String/String_1_using a sting.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 21:07:50 2021
+
+@author: shahi
+"""
+""" String Manupulation"""
+
+'''
+String can be in both single and double quote
+'''
+a = 'bangla'
+
+b = 'desh'
+
+print(a+b)
+
+a = "bangla"
+
+b = "desh"
+
+print(a+b)
+
+##If we use quotation inside a string it will show an error
+
+#c = 'I love my 'Motherland', it is Bangladesh'
+
+#so we use double quote insted of using single one 
+
+c = "I love my 'Motherland', it is Bangladesh"
+
+print (c)

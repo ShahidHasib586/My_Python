4fe4814497208424b996827c4f010bcc25ccff3c
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-22 22:15:32 +0600

    Add files via upload

---
+++ Level_1/Tuple/Tuple_1_Declaring a tuple.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 21:32:30 2021
+
+@author: shahi
+"""
+
+'''
+tuple is one kind of list except it is declare with () this bracket, tuple is can't not be changed as list
+'''
+
+a = ()
+type(a)
+
+#empty touple
+'''
+a touple can hold various datatype int, float, string, list, touple, set and other
+'''
+veg = ('onion', 'potato', 'ginger', 'cucumber')
+
+type(veg)
+
+'''
+mixed data type tuple
+'''
+
+cash = (8, 9 , 'money', 'note', (3, 4, 6), ['ball', 'bat', 'mouse'], 3.1416)
+
+type(cash)

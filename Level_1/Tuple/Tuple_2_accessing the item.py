a26ab3eb4ea638fd34f43b0496f0d9c6bd6f258c
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-22 23:10:58 +0600

    Add files via upload

---
+++ Level_1/Tuple/Tuple_2_accessing the item.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 21:32:30 2021
+
+@author: shahi
+"""
+
+'''
+accessing the tuple item
+'''
+veg = ('onion', 'potato', 'ginger', 'cucumber')
+cash = (8, 9 , 'money', 'note', (3, 4, 6), ['ball', 'bat', 'mouse'], 3.1416)
+
+veg[0]
+veg[1:4]
+veg[:4]
+veg[2:]
+#checking the item
+type(cash[4])
+type(cash[5])
+

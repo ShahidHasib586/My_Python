c3f05e4e0f238303c4aea6ad20f8e7ae1bd1b202
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-22 23:11:29 +0600

    Add files via upload

---
+++ Level_1/Tuple/Tuple_4_counting and finding item.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 21:32:30 2021
+
+@author: shahi
+"""
+
+'''
+Counting and Finding
+'''
+veg = ('onion', 'potato', 'ginger', 'cucumber')
+cash = (8, 3, 9 , 'money', 'note', 3, (3, 4, 6), ['ball', 'bat', 'mouse'], 3.1416)
+
+#we can use len() function to know the total item number of the tuple
+
+
+
+len(veg)
+
+
+'''
+we can use count funtion to konw the amount of any item appernce in the tuple
+'''
+cash.count((3))

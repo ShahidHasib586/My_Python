ca70989f70e98101c45660ed42f421170db92dfe
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-23 21:21:07 +0600

    Add files via upload

---
+++ Level_1/Set/Set_3_operation of a set.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 19:22:58 2021
+
+@author: shahi
+"""
+
+'''
+union, intersection, difference
+'''
+
+A = {'orange', 'banana', 'pineapple', 'apple', 'mango'}
+
+B = {'orange', 'mango', 'grape', 'berry'}
+
+#union
+A.union(B)
+
+#intersaction
+A.intersection(B)
+
+# Difference
+A.difference(B)

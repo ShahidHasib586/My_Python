49055cd0af92d06e5abae5fbb524a6fbaadc5540
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-31 23:43:39 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/If_Else_3_Problem_Solving.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 23:37:59 2021
+
+@author: shahi
+"""
+
+
+"""
+An user will give a input number, we need to check whether the input number is positive negative or zero
+"""
+
+print("Please Enter the Number")
+
+number = int(input())
+
+if number > 0:
+    print("You have entered a positive number")
+
+elif number < 0:
+    print("You have entered a Negative number")
+
+else:
+    print("You have entered Zero")
+    
+    

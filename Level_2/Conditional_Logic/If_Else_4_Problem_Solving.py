794b293459ce54b58a17ce823790bf4afb3c3ad4
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-05-31 23:49:34 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/If_Else_4_Problem_Solving.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 23:37:59 2021
+
+@author: shahi
+"""
+
+
+"""
+An user will give a input number, we need to check whether the input number is positive negative or zero
+"""
+
+print("Please Enter the Number")
+
+number = int(input())
+
+if number%2 == 0:
+    print("You have entered an Even number")
+
+
+else:
+    print("You have entered an Odd number")
+    
+    

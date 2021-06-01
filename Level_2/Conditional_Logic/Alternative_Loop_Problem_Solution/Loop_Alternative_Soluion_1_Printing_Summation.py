de8ad4c4929062c0f6c5b38562e91644c59484d9
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-02 00:48:33 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Alternative_Loop_Problem_Solution/Loop_Alternative_Soluion_1_Printing_Summation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 21:17:15 2021
+
+@author: shahi
+"""
+
+    
+# for eveery n value print the n value increases to n+1 unless it reaches to 10
+
+# when loop starts for first time the value of n is 1 than it is 2 .... when it is 11 the condition gets falues and loop stops
+
+'''
+Problem:2
+calculate the series sumation: 1+2+3+4+......+100=?(without using loop)
+'''
+n = 100
+
+sumation = (n*(n+1))/2
+
+print(sumation)
+
+#this solution is much efficient than the use of loops
+

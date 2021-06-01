37aa0addec172a6b8ae4f0a367d8213965ce326e
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-01 21:06:44 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/If_Else_5_Problem_Solving.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 23:37:59 2021
+
+@author: shahi
+"""
+
+
+"""
+An user will give a input number, we need to check whether the input number is positive negative or zero
+"""
+
+print("Please Enter the Alphabet")
+
+character = input()
+
+if character >='A' and character <='Z' :
+    print("You have entered an Upper Case character")
+
+
+elif character >='a' and character <='z':
+    print("You have entered an Lower Case character")
+
+else: 
+    
+    print("invalid input")
+    
+    

12126360a69680d46d7142e4816b77817dfe72b9
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-02 22:57:51 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Loop/for_Loop/Loop_5_for_loop_else_statement.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 20:31:35 2021
+
+@author: shahi
+"""
+
+'''
+While loop and else estatement
+'''
+
+for n in range (1, 11):
+    print(n)
+else:
+    print('loop is over')
+    
+    #alternative
+    
+for n in range (0, 10):
+    
+    n = n+1
+    print(n)
+else:
+    print('loop is over')
+    

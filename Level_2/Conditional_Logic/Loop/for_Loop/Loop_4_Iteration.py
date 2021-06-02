484d63aef14989ecb3874f03585529226edfa56e
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-02 06:44:22 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Loop/for_Loop/Loop_4_Iteration.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 05:05:20 2021
+
+@author: shahi
+
+"""
+'''
+Problem:4 print 1 to 0 using for loop
+'''
+
+'''
+we can use range function for this 
+'''
+range(1, 91)
+list(range(1, 91))
+
+list(range(0, 101, 5))
+
+#problem 4:
+    
+for n in range (0, 10):
+    n=n+1
+    print(n)
+    
+#or
+for n in range (1, 11):
+    print(n)
+
+
+    
+
+    

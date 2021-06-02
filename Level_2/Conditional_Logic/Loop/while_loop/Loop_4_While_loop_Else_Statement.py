acb3be7915a6731a583d106e7b762a90f8b4b3e8
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-02 22:58:22 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Loop/while_loop/Loop_4_While_loop_Else_Statement.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 20:31:35 2021
+
+@author: shahi
+"""
+
+'''
+While loop and else estatement
+'''
+
+n =1
+while n<=10:
+    
+    print(n)
+    n=n+1
+else:
+    print('loop is over')

df3559aac63b50b5e1f6dab1eda30dacd78f4a65
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-02 22:59:57 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Loop/Loop_Control_Statement/Break.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 19:44:32 2021
+
+@author: shahi
+"""
+'''
+To control a loop we use loop control statement
+'''
+# Break
+
+'''When loop finds break statement inside a loop it stops executing the loop and goes for next statement'''
+
+for num in range (1, 11):
+    
+    if num == 5:
+        break
+    print(num)
+    
+''' when number is equal to 5 loop is breaked'''

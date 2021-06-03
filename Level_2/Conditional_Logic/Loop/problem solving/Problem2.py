2ad74c920c8645884e18dccba2a0f1abf3653420
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-03 07:40:08 +0600

    Add files via upload

---
+++ Level_2/Conditional_Logic/Loop/problem solving/Problem2.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 06:31:42 2021
+
+@author: shahi
+"""
+
+'''
+print star rectangle from user input : *****
+                                       *****
+                                       *****
+                                       *****
+                                       ***** (if input=5)
+'''
+
+
+#__________________________________________________________________
+#solve(while loop)
+print('Enter the number:\n')
+
+number = int(input())
+
+temp = number
+
+while number > 0:
+    
+    count = temp
+   
+    while count > 0:
+       
+        print('*', end ='')
+        
+        count -=1
+        
+    print()
+    
+    number -= 1
+
+
+#__________________________________________________________________    
+#solve(for loop)
+
+
+print('Enter the number:\n')
+
+number = int(input())
+
+temp = number
+
+for n in range (number):
+    
+    count = temp
+    
+    for i in range (count):
+        
+        print('*', end='')
+        
+        count-=1
+    print()
+    number-=1
+
+#__________________________________________________________________    
+    
+#solve(without decrement for loop)   
+
+print('Enter the number:\n')
+
+number = int(input())
+
+for i in range (number):
+    for j in range (number):
+        
+        print('*', end='')
+    print() 
+
+#__________________________________________________________________
+
+#solve(without nested loop)
+
+print('Enter the number:\n')
+
+number = int(input())
+
+for i in range (number):
+    print('*'*number)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        

6de923ea723508828cf04edfd42f92b7427e0390
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-06-13 03:46:31 +0600

    Add files via upload

---
+++ Level_3/Function/Function_1_introduction.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 13 02:35:39 2021
+
+@author: shahi
+"""
+
+'''
+in functon there are three things we need to be concerned about: 1.Name, 2.parameter, 3.Return
+function can take multiple parameter but returns only one 
+in python function are organised and reusable. Functions can be divided in to two types
+1. Built-in and user defined: print(), input(), pop(), they are bult in functions. 
+
+Some importent built in function in python:
+    
+    1.abs(): parameter: intiger/float. returns: absolute value
+    2.all(): parameter: iterable. ruturns: True when iterable is empty or fevery item in iterable is True
+    3.any(): parameter: iterable: returns: If any of item is true then returns True else returns False
+    4.enumerate(): parameter: iterable and start = 0, returns: enumerate objects where every iterable item and count stays as tuple 
+Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object. 
+This enumerate object can then be used directly in for loops or be converted into a list of tuples using list() method.
+'''
+

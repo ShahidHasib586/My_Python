ac7af7747df3730ece0160da16e6f1ed2c9635b7
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-07-12 22:57:10 +0600

    Add files via upload

---
+++ Level_3/Function/Function_2_1st function.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 06:55:45 2021
+
+@author: shahi
+"""
+
+def my_identity(my_name, roll):
+    
+    print('My name is:', my_name)
+    print('and roll is:', roll)
+    return
+my_identity('Shahid Ahamed Hasib', '1608014')
+
+
+   

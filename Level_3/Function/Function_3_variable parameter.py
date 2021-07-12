17c1848fdaed3644da0b7bc845ab08fd05cde14b
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2021-07-12 22:57:31 +0600

    Add files via upload

---
+++ Level_3/Function/Function_3_variable parameter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 07:13:56 2021
+
+@author: shahi
+"""
+
+def my_identity(my_name, roll):
+    
+    print('My name is:', my_name)
+    print('and roll is:', roll)
+    return
+Name = 'Shahid Ahamed Hasib'
+Roll = '1608014'
+my_identity(Name, Roll)
+

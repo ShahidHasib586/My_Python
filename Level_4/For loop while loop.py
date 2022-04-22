df7079dc1358d7b22da100168302ebd24e7722c1
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2022-04-23 05:57:42 +0600

    Add files via upload

---
+++ Level_4/For loop while loop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 05:41:24 2022
+
+@author: shahi
+"""
+'''
+name = None
+while not name:
+    name = input("Enter Your Name: ")
+    
+print("Hello "+name)
+'''
+
+import time 
+
+for seconds in range (10,0,-1):
+    
+    print(seconds)
+    time.sleep(1)
+    
+print("Happy New Year")
+

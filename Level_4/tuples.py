38978cdb1414e219fe71468d8d854a98de6cce78
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2022-04-23 07:59:09 +0600

    Add files via upload

---
+++ Level_4/tuples.py
@@ -0,0 +1,18 @@
+'''collection which is uncangable and orderedultiple items in one varibale
+
+'''
+"""
+
+"""
+
+student = ('bro', 21, 'male')
+
+print(student.count("bro"))
+
+print(student.index("male"))
+
+for x in student:
+    print(x)
+    
+if 'bro' in student:
+    print('bro is here!')

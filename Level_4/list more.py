182b50fe9535df54590a017168f1d455d97f1294
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2022-04-23 07:44:58 +0600

    Add files via upload

---
+++ Level_4/list more.py
@@ -0,0 +1,27 @@
+'''
+List =  multiple items in one varibale
+
+'''
+"""
+food = ['pizza', 'pant', 'hot dog', 'pen']
+food[0]='clock'
+print(food[0:3:2])
+
+food.append('cock')
+food.remove('pant')
+food.pop()
+food.sort()
+food.insert(0,"money")
+for x in food:
+    print(x)
+"""
+
+#2D list
+
+drinks = ['coffee', 'soda', 'tea']
+dinner = ['pizza', 'hamburger', 'hot dog']
+dessert = ['cake', 'ice cream']
+
+food = [drinks, dinner, dessert]
+
+print(food[2][1])

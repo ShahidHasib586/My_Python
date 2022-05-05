ab139ca65b152d059bed294f8643209a107fae56
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2022-05-05 15:49:23 +0600

    Add files via upload

---
+++ Level_5/Fucntions 2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 15:33:52 2022
+
+@author: shahi
+"""
+
+#functions in python = a function is a block of code which is executed when it is called
+
+def hello(first_name, last_name, age):
+    print("hello! "+first_name+" "+last_name)
+    print("you are "+str(age)+" years old")
+    print("Have a nice day! ")
+hello("Bro","code", 21)
+
+#function need a matching number of parameter and arguments

72383d3d5f28ea36df02b169a0309460cdd2c52c
Shahid Ahamed Hasib <45465308+ShahidHasib586@users.noreply.github.com>
2022-05-16 01:23:37 +0600

    Add files via upload

---
+++ Level_5/fucntion_3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 23:25:13 2022
+
+@author: shahi
+"""
+
+def hello(name):
+    print("Hello! "+name)
+    print("Have a nice day!")
+    
+my_name = "Hasib"
+hello(my_name)
